141d195a2c4ccbcd6e0747e9b6b003884637ed3c
Héctor Francés <32682445+hfrances@users.noreply.github.com>
2024-09-22 15:35:38 +0200

    feat: Add SetAllMaps

--- qckdev.Data.Dapper/SqlMapperHelper.cs
+++ qckdev.Data.Dapper/SqlMapperHelper.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using System;
 using System.Linq;
+using System.Reflection;
 
 namespace qckdev.Data.Dapper
 {
@@ -11,6 +12,53 @@ namespace qckdev.Data.Dapper
     /// </summary>
     public static class SqlMapperHelper
     {
+
+        /// <summary>
+        /// Mapea los tipos dentro del ensamblado indicado para que Dapper utilice los atributos <see cref="TableAttribute"/> y <see cref="ColumnAttribute"/>
+        /// como nombres de tabla y columnas en las consultas SQL que genera.
+        /// <br/>
+        /// Busca en el ensamblado del tipo indicado.
+        /// <br/>
+        /// Importante: sólo mapea las clases con el atributo <see cref="TableAttribute"/>.
+        /// </summary>
+        public static void SetAllMaps<TAnchor>()
+        {
+            SetAllMaps(typeof(TAnchor));
+        }
+
+        /// <summary>
+        /// Mapea los tipos dentro del ensamblado indicado para que Dapper utilice los atributos <see cref="TableAttribute"/> y <see cref="ColumnAttribute"/>
+        /// como nombres de tabla y columnas en las consultas SQL que genera.
+        /// <br/>
+        /// Busca en el ensamblado del tipo indicado.
+        /// <br/>
+        /// Importante: sólo mapea las clases con el atributo <see cref="TableAttribute"/>.
+        /// </summary>
+        public static void SetAllMaps(Type anchorType)
+        {
+            var assembly = Assembly.GetAssembly(anchorType);
+            SetAllMaps(assembly);
+        }
+
+        /// <summary>
+        /// Mapea los tipos dentro del ensamblado indicado para que Dapper utilice los atributos <see cref="TableAttribute"/> y <see cref="ColumnAttribute"/>
+        /// como nombres de tabla y columnas en las consultas SQL que genera.
+        /// <br/>
+        /// Importante: sólo mapea las clases con el atributo <see cref="TableAttribute"/>.
+        /// </summary>
+        public static void SetAllMaps(params Assembly[] assemblies)
+        {
+            foreach (var ass in assemblies)
+            {
+                foreach (Type item in ass.GetTypes())
+                {
+                    if (item.GetCustomAttributes<TableAttribute>().Any())
+                    {
+                        SetMapper(item);
+                    }
+                }
+            }
+        }
 
         /// <summary>
         ///  Set custom mapping for a specific type.
